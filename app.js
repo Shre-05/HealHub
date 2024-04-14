e274c017b8cb09848690d14444a08da0218d6756
Shreya Ghodake <144779803+Shre-05@users.noreply.github.com>
2024-04-14 17:06:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+// app.js
+document.getElementById('logoutBtn').addEventListener('click', function() {
+    // Implement logout functionality here
+    alert('Logout functionality will be implemented here');
+  });
+  
